083cdd57d1682360cf0c75bacb997e851874fd2a
zlateska <hzlateska@gmail.com>
2022-10-21 12:06:48 +0200

    Refactor and add docs bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,28 +1,33 @@
 <?php
+/**
+ * Newfold Labs WordPress Patterns Module.
+ */
 
 use NewfoldLabs\WP\ModuleLoader\Container;
 use NewfoldLabs\WP\Module\Patterns\Patterns;
 use function NewfoldLabs\WP\ModuleLoader\register;
 
-if ( function_exists( 'add_action' ) ) {
+/**
+ * Register patterns module with Newfold Labs Module Loader.
+ */
+function nfd_wp_module_patterns_register() {
+	register(
+		array(
+			'name'     => 'wp-module-patterns',
+			'label'    => __( 'Patterns', 'nfd-wp-module-patterns' ),
+			'callback' => function ( Container $container ) {
 
-	add_action(
-		'plugins_loaded',
-		function () {
-
-			register(
-				[
-					'name'     => 'wp-module-patterns',
-					'label'    => __( 'wp module patterns', 'newfold-wp-module-patterns-module' ),
-					'callback' => function ( Container $container ) {
-						new Patterns( $container );
-					},
-					'isActive' => true,
-					'isHidden' => true,
-				]
-			);
-
-		}
+				new Patterns( $container );
+			},
+			'isActive' => true,
+			'isHidden' => true,
+		)
 	);
+}
 
+/**
+ * Tap WordPress Hooks to Instantiate Module Loader
+ */
+if ( is_callable( 'add_action' ) ) {
+	add_action( 'plugins_loaded', 'nfd_wp_module_patterns_register' );
 }
