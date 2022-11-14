741555e284089070cd878ba05bd3a1f21f753078
zlateska <hzlateska@gmail.com>
2022-11-14 12:17:08 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,36 @@
 const tailwind = require('./tailwind.config');
+const { resolve } = require('path');
 
-module.exports = {
-	plugins: {
-		tailwindcss: {
+module.exports = (env) => ({
+	ident: 'postcss',
+	sourceMap: env !== 'production',
+	plugins: [
+		require('tailwindcss')({
 			...tailwind,
-			// important: '.nfd-cloud-patterns',
+			config: resolve(__dirname, 'tailwind.config.js'),
+		}),
+		(css) => {
+			css.walkRules((rule) => {
+				// Remove global rules like * and ::backdrop.
+				if (
+					rule.selector.startsWith('*') ||
+					rule.selector.startsWith('::backdrop')
+				) {
+					rule.remove();
+				}
+			});
 		},
-		autoprefixer: {
-			grid: true,
-		},
-	},
-};
+		require('autoprefixer')({ grid: true }),
+		env === 'production' &&
+			require('cssnano')({
+				preset: [
+					'default',
+					{
+						discardComments: {
+							removeAll: true,
+						},
+					},
+				],
+			}),
+	],
+});
