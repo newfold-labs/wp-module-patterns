784b2a1c9829d4d4861ed24516cb09457f060c2c
Branko Conjic <brankoconjic@gmail.com>
2022-11-19 13:00:41 +0100

    Fix typography utility classes

--- css-utilities/tailwind.config.js
+++ css-utilities/tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
 		preflight: false,
 	},
 
+	theme: {
+		screens: {
+			md: { max: '782px' },
+			// => @media (max-width: 782px) { ... }
+		},
+	},
+
 	// Include utility classes we need.
-	safelist: ['nfd-my-0', 'nfd-px-4'],
+	safelist: [
+		'nfd-my-0',
+		'nfd-px-4',
+		'nfd-p-5',
+		'md:nfd-hidden',
+		'md:nfd-order-2',
+	],
 };
