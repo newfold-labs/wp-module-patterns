44f2b15af76caf85fded196e6e0dca1730dd273a
Branko Conjic <brankoconjic@gmail.com>
2022-11-22 12:52:37 +0100

    Add utility classes

--- css-utilities/tailwind.config.js
+++ css-utilities/tailwind.config.js
@@ -14,9 +14,15 @@ module.exports = {
 
 	// Include utility classes we need.
 	safelist: [
-		'nfd-my-0',
-		'nfd-px-6',
+		'nfd-p-0',
 		'nfd-p-5',
+		'nfd-py-0',
+		'nfd-py-5',
+		'nfd-px-6',
+		'nfd-my-0',
+		'nfd-relative',
+		'-nfd-top-1',
+		'md:nfd-py-0',
 		'md:nfd-hidden',
 		'md:nfd-order-2',
 	],
