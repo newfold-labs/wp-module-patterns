718a6c34da728324f3c2c8ea713e0edfbea1eeb6
Branko Conjic <brankoconjic@gmail.com>
2023-02-20 12:18:01 +0100

    Update ErrorLoading

--- src/components/ErrorLoading.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * WordPress dependencies
- */
-import { __ } from '@wordpress/i18n';
-
-const ErrorLoading = () => {
-	return <p>{__('Failed to load data.', 'nfd-wonder-blocks')}</p>;
-};
-export default ErrorLoading;
