27df131ad0ce74a02ada747a0ef526a0b7e84d9e
Branko Conjic <brankoconjic@gmail.com>
2023-02-21 00:09:21 +0100

    Disable input when content is loading

--- src/components/Modal/Content/Header/KeywordFilter.jsx
+++ src/components/Modal/Content/Header/KeywordFilter.jsx
@@ -1,10 +1,10 @@
 /**
  * WordPress dependencies
  */
-import { useRef, useState, useEffect } from '@wordpress/element';
 import { SearchControl } from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { useEffect, useRef, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { useDispatch } from '@wordpress/data';
 
 /**
  * External dependencies
@@ -14,14 +14,18 @@ import debounce from 'lodash/debounce';
 /**
  * Internal dependencies
  */
-import { store as nfdPatternsStore } from '../../../../store';
 import { INPUT_DEBOUNCE_TIME } from '../../../../constants';
+import { store as nfdPatternsStore } from '../../../../store';
 
 const KeywordFilter = () => {
 	const searchRef = useRef('');
 	const [searchValue, setSearchValue] = useState('');
 
 	const { setKeywordsFilter } = useDispatch(nfdPatternsStore);
+	const { isSidebarLoading, isContentLoading } = useSelect((select) => ({
+		isSidebarLoading: select(nfdPatternsStore).isSidebarLoading(),
+		isContentLoading: select(nfdPatternsStore).isContentLoading(),
+	}));
 
 	// Focus the search input on mount.
 	useEffect(() => {
@@ -46,6 +50,7 @@ const KeywordFilter = () => {
 		<div className="nfd-wba-flex nfd-wba-items-center nfd-wba-gap-x-3">
 			<SearchControl
 				className="nfd-wba-keyword-filter nfd-wba-m-0"
+				disabled={isSidebarLoading || isContentLoading}
 				placeholder={__('Keywords', 'nfd-wonder-blocks')}
 				ref={searchRef}
 				value={searchValue}
