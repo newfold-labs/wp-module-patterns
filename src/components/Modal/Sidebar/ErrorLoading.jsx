d60f25bc1b1acac2a72cc38d85e2f2ec97b0696f
Branko Conjic <brankoconjic@gmail.com>
2023-02-20 12:20:10 +0100

    Create ErrorLoading.jsx

---
+++ src/components/Modal/Sidebar/ErrorLoading.jsx
@@ -0,0 +1,13 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+const ErrorLoading = () => {
+	return (
+		<p className="nfd-wba-m-0 nfd-wba-px-6 nfd-wba-pt-[5vh] nfd-wba-text-center">
+			{__('Failed to load data.', 'nfd-wonder-blocks')}
+		</p>
+	);
+};
+export default ErrorLoading;
