e07aa2726a474fca96949872b7c8889ef7d44fd6
zlateska <hzlateska@gmail.com>
2023-02-23 11:22:45 +0100

    Update design Block preview

--- src/components/Modal/Content/Content.jsx
+++ src/components/Modal/Content/Content.jsx
@@ -8,13 +8,14 @@ import { sprintf, __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
+import useFavorites from '../../../hooks/useFavorites';
 import usePatterns from '../../../hooks/usePatterns';
 import { store as nfdPatternsStore } from '../../../store';
 import DesignList from './DesignList/DesignList';
 import Header from './Header/Header';
 import LoadingBar from './LoadingBar';
 
-const Content = ({ selectedTab }) => {
+const Content = ({ selectedTab = 'patterns' }) => {
 	const {
 		activePatternsCategory,
 		activeTemplatesCategory,
@@ -31,7 +32,13 @@ const Content = ({ selectedTab }) => {
 		keywordsFilter: select(nfdPatternsStore).getKeywordsFilter(),
 	}));
 
+	// Fetch data.
 	const { data, isValidating } = usePatterns('test');
+	const { data: favData } = useFavorites(selectedTab);
+
+	// Fetch favorites (type = selectedTab)
+	// const { data: favoritesData } = useFavorites(selectedTab); // u sebi treba da ima useEffect koji na promenu data izvrsava dispatch
+
 	const { setIsContentLoading } = useDispatch(nfdPatternsStore);
 
 	// Set the global content loading state when the data is loading.
@@ -42,6 +49,10 @@ const Content = ({ selectedTab }) => {
 	return (
 		<div className="nfd-wba-flex nfd-wba-min-w-[400px] nfd-wba-grow nfd-wba-flex-col nfd-wba-overflow-y-auto">
 			<Header />
+
+			<pre className="nfd-wba-whitespace-pre-wrap nfd-wba-bg-grey-b nfd-wba-p-6">
+				{JSON.stringify(favData, null, 2)}
+			</pre>
 
 			<div className="nfd-wba-relative nfd-wba-flex nfd-wba-grow nfd-wba-flex-col nfd-wba-gap-y-10">
 				{<LoadingBar isComplete={data} />}
@@ -60,21 +71,21 @@ const Content = ({ selectedTab }) => {
 					{data && <DesignList data={data} />}
 
 					{
-						<pre className="nfd-wba-m-0 nfd-wba-whitespace-pre-wrap nfd-wba-rounded-md nfd-wba-bg-grey nfd-wba-p-6">
-							{JSON.stringify(
-								{
-									selectedTab,
-									activePatternsCategory,
-									activeTemplatesCategory,
-									isContentLoading,
-									isSidebarLoading,
-									keywordsFilter,
-									data,
-								},
-								null,
-								2
-							)}
-						</pre>
+						// <pre className="nfd-wba-m-0 nfd-wba-whitespace-pre-wrap nfd-wba-rounded-md nfd-wba-bg-grey nfd-wba-p-6">
+						// 	{JSON.stringify(
+						// 		{
+						// 			selectedTab,
+						// 			activePatternsCategory,
+						// 			activeTemplatesCategory,
+						// 			isContentLoading,
+						// 			isSidebarLoading,
+						// 			keywordsFilter,
+						// 			data,
+						// 		},
+						// 		null,
+						// 		2
+						// 	)}
+						// </pre>
 					}
 				</div>
 			</div>
--- src/components/Modal/Content/DesignList/DesignItem.jsx
+++ src/components/Modal/Content/DesignList/DesignItem.jsx
@@ -1,11 +1,16 @@
 /**
  * WordPress dependencies
  */
+import apiFetch from '@wordpress/api-fetch';
+import { BlockPreview } from '@wordpress/block-editor';
 import { rawHandler } from '@wordpress/blocks';
+import { Button } from '@wordpress/components';
 import { useDispatch } from '@wordpress/data';
-import { useMemo } from '@wordpress/element';
+import { memo, useMemo } from '@wordpress/element';
 import { sprintf, __ } from '@wordpress/i18n';
+import { Icon, plus, starEmpty, starFilled } from '@wordpress/icons';
 import { store as noticesStore } from '@wordpress/notices';
+import { REST_URL } from '../../../../constants';
 
 /**
  * Internal dependencies
@@ -35,7 +40,10 @@ const DesignItem = ({ item }) => {
 			createSuccessNotice(
 				sprintf(
 					// translators: %s is the pattern title
-					__('"%s" pattern successfully added.', 'nfd-wonder-blocks'),
+					__(
+						'"%s" pattern successfully inserted.',
+						'nfd-wonder-blocks'
+					),
 					item.title
 				),
 				{
@@ -58,20 +66,69 @@ const DesignItem = ({ item }) => {
 		}
 	};
 
+	const addToFavoritesHandler = async () => {
+		await apiFetch({
+			url: `${REST_URL}/favorites/patterns`,
+			method: 'POST',
+			data: item,
+			headers: {
+				'x-nfd-wonder-blocks': 'nfd_wonder_blocks',
+			},
+		});
+	};
+
 	return (
-		<button
-			type="button"
-			onClick={() => {
-				insertDesignHandler();
-			}}
-			onKeyUp={(e) => {
-				if (e.key === 'Enter') {
-					insertDesignHandler();
-				}
-			}}
-		>
-			{item.title}
-		</button>
+		<div className="nfd-wba-relative nfd-wba-mb-[var(--nfd-wba-masonry-gap)] nfd-wba-flex nfd-wba-flex-col nfd-wba-overflow-hidden nfd-wba-rounded-b-md">
+			<div
+				className="nfd-wba-cursor-pointer nfd-wba-overflow-hidden nfd-wba-rounded-t-md nfd-wba-border nfd-wba-border-solid nfd-wba-border-grey"
+				role="button"
+				tabIndex="0"
+				onClick={() => insertDesignHandler()}
+				onKeyUp={(e) => {
+					if (e.key === 'Enter') {
+						insertDesignHandler();
+					}
+				}}
+			>
+				{blocks && (
+					<BlockPreview blocks={blocks} viewportWidth={1140} />
+				)}
+			</div>
+
+			<div className="nfd-wba-flex nfd-wba-items-center nfd-wba-justify-between nfd-wba-gap-3 nfd-wba-bg-grey nfd-wba-px-6 nfd-wba-py-3">
+				<h2 className="nfd-wba-m-0 nfd-wba-text-md nfd-wba-font-normal">
+					{item.title}
+				</h2>
+
+				<div className="nfd-wba-flex nfd-wba-shrink-0 nfd-wba-items-center nfd-wba-gap-1">
+					<Button
+						className="nfd-wba-h-8 nfd-wba-w-8 !nfd-wba-min-w-0 nfd-wba-bg-white"
+						label={__('Insert', 'nfd-wonder-blocks')}
+						icon={
+							<Icon
+								fill="currentColor"
+								className="nfd-wba-shrink-0"
+								icon={plus}
+								onClick={() => insertDesignHandler()}
+							/>
+						}
+					/>
+
+					<Button
+						className="nfd-wba-h-8 nfd-wba-w-8 !nfd-wba-min-w-0 nfd-wba-bg-white"
+						label={__('Add to Favorites', 'nfd-wonder-blocks')}
+						icon={
+							<Icon
+								fill="currentColor"
+								className="nfd-wba-shrink-0"
+								icon={starEmpty}
+								onClick={() => addToFavoritesHandler()}
+							/>
+						}
+					/>
+				</div>
+			</div>
+		</div>
 	);
 };
-export default DesignItem;
+export default memo(DesignItem);
