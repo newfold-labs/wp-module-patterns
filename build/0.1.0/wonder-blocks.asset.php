af28a48579abc6d89a243cc55fdf7db0f7b458b0
Branko Conjic <brankoconjic@gmail.com>
2023-03-03 17:35:47 +0100

    Build

--- build/0.1.0/wonder-blocks.asset.php
+++ build/0.1.0/wonder-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => '58cb81791b6db11f66c5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => 'be11a77bb0f41c9c79a5');
