73e3ac066da4c3aaffc2d479b3fbd87e27454347
Branko Conjic <brankoconjic@gmail.com>
2023-03-06 14:58:04 +0100

    Delete build/0.1.0 directory

--- build/0.1.0/wonder-blocks.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => '03b57796c79df8e6709b');
