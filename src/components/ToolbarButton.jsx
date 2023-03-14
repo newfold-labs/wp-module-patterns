bc7710ec3ecf595218158d7aac4d8f897b0cb6e4
zlateska <hzlateska@gmail.com>
2023-03-14 21:14:15 +0100

    Fix notice about Toolbar component

--- src/components/ToolbarButton.jsx
+++ src/components/ToolbarButton.jsx
@@ -1,10 +1,14 @@
 /**
+ * External dependencies
+ */
+import classNames from 'classnames';
+
+/**
  * WordPress dependencies
  */
-import { Button } from '@wordpress/components';
+import { ToolbarButton as WPToolbarButton } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { buttons, Icon } from '@wordpress/icons';
-import classNames from 'classnames';
 
 /**
  * Internal dependencies
@@ -19,17 +23,15 @@ const ToolbarButton = () => {
 	const { setIsModalOpen } = useDispatch(nfdPatternsStore);
 
 	return (
-		<>
-			<Button
-				icon={<Icon icon={buttons} />}
-				text="Wonder Blocks"
-				className={classNames(
-					'nfd-wba-ml-2 nfd-wba-flex nfd-wba-h-9 nfd-wba-shrink-0 nfd-wba-gap-1 nfd-wba-bg-brand nfd-wba-text-white hover:nfd-wba-bg-brand-darker hover:nfd-wba-text-white focus-visible:nfd-wba-text-white active:!nfd-wba-text-white'
-				)}
-				isPressed={isModalOpen}
-				onClick={() => setIsModalOpen(true)}
-			/>
-		</>
+		<WPToolbarButton
+			icon={<Icon icon={buttons} />}
+			text="Wonder Blocks"
+			className={classNames(
+				'nfd-wba-ml-2 nfd-wba-flex !nfd-wba-h-9 nfd-wba-shrink-0 nfd-wba-gap-1 nfd-wba-bg-brand nfd-wba-text-white hover:nfd-wba-bg-brand-darker hover:nfd-wba-text-white focus-visible:nfd-wba-text-white active:nfd-wba-bg-brand-darker-10 active:!nfd-wba-text-white'
+			)}
+			isPressed={isModalOpen}
+			onClick={() => setIsModalOpen(true)}
+		/>
 	);
 };
 
