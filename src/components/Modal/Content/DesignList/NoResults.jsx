76b0e2a14e14d682c4cbeab2ec6baef55f426d7e
zlateska <hzlateska@gmail.com>
2023-03-23 15:25:34 +0100

    Add No Results screens

--- src/components/Modal/Content/DesignList/NoResults.jsx
+++ src/components/Modal/Content/DesignList/NoResults.jsx
@@ -3,7 +3,34 @@
  */
 import { __ } from '@wordpress/i18n';
 
-const NoResults = () => {
-	return <div>{__('No results found', 'nfd-wonder-blocks')}</div>;
+/**
+ * Internal dependencies
+ */
+import { ReactComponent as NoResultsSVG } from '../../../../svg/NoResults.svg';
+import { ReactComponent as NoFavoritesSVG } from '../../../../svg/NoFavorites.svg';
+
+const NoResults = ({ isFavorites }) => {
+	const title = isFavorites
+		? __(
+				"You haven't added any patterns or page templates to your favorites yet.",
+				'nfd-wonder-blocks'
+		  )
+		: __(
+				"Sorry, we couldn't find any results for that. Please try a different search term.",
+				'nfd-wonder-blocks'
+		  );
+
+	const svg = isFavorites ? <NoFavoritesSVG /> : <NoResultsSVG />;
+
+	return (
+		<div className="nfd-wba-flex nfd-wba-grow nfd-wba-items-center nfd-wba-justify-center">
+			<div className="nfd-wba-w-[640px] nfd-wba-gap-8 nfd-wba-pb-[10%]">
+				{svg}
+				<p className="nfd-wba-text-center nfd-wba-text-2xl nfd-wba-font-light nfd-wba-text-dark">
+					{title}
+				</p>
+			</div>
+		</div>
+	);
 };
 export default NoResults;
