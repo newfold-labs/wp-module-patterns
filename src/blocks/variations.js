ccbcf3991de5f1c785a088cf1001489d84db8ce0
Branko Conjic <brankoconjic@gmail.com>
2023-03-28 20:01:57 +0200

    Add block variation keywords

--- src/blocks/variations.js
+++ src/blocks/variations.js
@@ -27,8 +27,9 @@ export const variations = [
 		title: __('Gallery Patterns', 'nfd-wonder-blocks'),
 		description: __('Add gallery patterns.', 'nfd-wonder-blocks'),
 		keywords: [
-			__('gallery', 'nfd-wonder-blocks'),
 			__('images', 'nfd-wonder-blocks'),
+			__('photos', 'nfd-wonder-blocks'),
+			__('photography', 'nfd-wonder-blocks'),
 		],
 	},
 	{
@@ -41,7 +42,11 @@ export const variations = [
 		attributes: { category: 'blog' },
 		title: __('Blog Patterns', 'nfd-wonder-blocks'),
 		description: __('Add blog patterns.', 'nfd-wonder-blocks'),
-		keywords: [__('blog', 'nfd-wonder-blocks')],
+		keywords: [
+			__('articles', 'nfd-wonder-blocks'),
+			__('posts', 'nfd-wonder-blocks'),
+			__('news', 'nfd-wonder-blocks'),
+		],
 	},
 	{
 		name: 'call-to-action',
@@ -53,7 +58,11 @@ export const variations = [
 		attributes: { category: 'cta' },
 		title: __('Call to Action Patterns', 'nfd-wonder-blocks'),
 		description: __('Add Call to Action patterns.', 'nfd-wonder-blocks'),
-		keywords: [__('gallery', 'nfd-wonder-blocks')],
+		keywords: [
+			__('cta', 'nfd-wonder-blocks'),
+			__('conversion', 'nfd-wonder-blocks'),
+			__('button', 'nfd-wonder-blocks'),
+		],
 	},
 	{
 		name: 'faq',
@@ -65,7 +74,7 @@ export const variations = [
 		attributes: { category: 'faq' },
 		title: __('FAQ Patterns', 'nfd-wonder-blocks'),
 		description: __('Add FAQ patterns.', 'nfd-wonder-blocks'),
-		keywords: [__('gallery', 'nfd-wonder-blocks')],
+		keywords: [__('frequently asked questions', 'nfd-wonder-blocks')],
 	},
 	{
 		name: 'features',
@@ -77,7 +86,10 @@ export const variations = [
 		attributes: { category: 'features' },
 		title: __('Features Patterns', 'nfd-wonder-blocks'),
 		description: __('Add Features patterns.', 'nfd-wonder-blocks'),
-		keywords: [__('gallery', 'nfd-wonder-blocks')],
+		keywords: [
+			__('columns', 'nfd-wonder-blocks'),
+			__('about', 'nfd-wonder-blocks'),
+		],
 	},
 	{
 		name: 'headings',
@@ -89,7 +101,12 @@ export const variations = [
 		attributes: { category: 'headings' },
 		title: __('Heading Patterns', 'nfd-wonder-blocks'),
 		description: __('Add Heading patterns.', 'nfd-wonder-blocks'),
-		keywords: [__('gallery', 'nfd-wonder-blocks')],
+		keywords: [
+			__('title', 'nfd-wonder-blocks'),
+			__('headline', 'nfd-wonder-blocks'),
+			__('tagline', 'nfd-wonder-blocks'),
+			__('text', 'nfd-wonder-blocks'),
+		],
 	},
 	{
 		name: 'hero',
@@ -102,8 +119,9 @@ export const variations = [
 		title: __('Hero Patterns', 'nfd-wonder-blocks'),
 		description: __('Add Hero patterns.', 'nfd-wonder-blocks'),
 		keywords: [
-			__('gallery', 'nfd-wonder-blocks'),
-			__('images', 'nfd-wonder-blocks'),
+			__('banner', 'nfd-wonder-blocks'),
+			__('image slider', 'nfd-wonder-blocks'),
+			__('homepage', 'nfd-wonder-blocks'),
 		],
 	},
 	{
@@ -117,8 +135,9 @@ export const variations = [
 		title: __('Pricing Table Patterns', 'nfd-wonder-blocks'),
 		description: __('Add Pricing Table patterns.', 'nfd-wonder-blocks'),
 		keywords: [
-			__('gallery', 'nfd-wonder-blocks'),
-			__('images', 'nfd-wonder-blocks'),
+			__('plans', 'nfd-wonder-blocks'),
+			__('comparison', 'nfd-wonder-blocks'),
+			__('packages', 'nfd-wonder-blocks'),
 		],
 	},
 	{
@@ -132,8 +151,9 @@ export const variations = [
 		title: __('Team Patterns', 'nfd-wonder-blocks'),
 		description: __('Add Team patterns.', 'nfd-wonder-blocks'),
 		keywords: [
-			__('gallery', 'nfd-wonder-blocks'),
-			__('images', 'nfd-wonder-blocks'),
+			__('employees', 'nfd-wonder-blocks'),
+			__('members', 'nfd-wonder-blocks'),
+			__('profiles', 'nfd-wonder-blocks'),
 		],
 	},
 	{
@@ -147,8 +167,26 @@ export const variations = [
 		title: __('Testimonial Patterns', 'nfd-wonder-blocks'),
 		description: __('Add Testimonial patterns.', 'nfd-wonder-blocks'),
 		keywords: [
-			__('gallery', 'nfd-wonder-blocks'),
-			__('images', 'nfd-wonder-blocks'),
+			__('reviews', 'nfd-wonder-blocks'),
+			__('feedback', 'nfd-wonder-blocks'),
+			__('ratings', 'nfd-wonder-blocks'),
+		],
+	},
+	{
+		name: 'favorites',
+		icon: {
+			foreground: 'var(--nfd-wba-color-brand)',
+			src: quote,
+		},
+		category: 'nfd-wonder-blocks',
+		attributes: { category: 'favorites' },
+		title: __('My Favorite Patterns', 'nfd-wonder-blocks'),
+		description: __('Add Testimonial patterns.', 'nfd-wonder-blocks'),
+		keywords: [
+			__('liked', 'nfd-wonder-blocks'),
+			__('saved', 'nfd-wonder-blocks'),
+			__('bookmarked', 'nfd-wonder-blocks'),
+			__('starred', 'nfd-wonder-blocks'),
 		],
 	},
 ];
