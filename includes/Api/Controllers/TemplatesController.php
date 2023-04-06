76bd7ea6bc6767f437a60129433a9f4842644790
zlateska <hzlateska@gmail.com>
2023-04-06 12:14:24 +0200

    Update TemplatesController.php

--- includes/Api/Controllers/TemplatesController.php
+++ includes/Api/Controllers/TemplatesController.php
@@ -11,13 +11,25 @@ class TemplatesController {
 	public static function index( $request ) {
 	
 		$params = $request->get_query_params();
-
+		
+		$args = array();
+		
+		if ( isset( $params['category'] ) ) {
+			$args['category_like'] = sanitize_text_field( $params['category'] );
+		}
+		
+		if ( isset( $params['keywords'] ) ) {
+			$args['keywords_like'] = sanitize_text_field( $params['keywords'] );
+		}
+		
 		$response = RestRequest::get(
 			'/templates',
-			array(
-				'category' => isset( $params['category'] ) ? sanitize_text_field( $params['category'] ) : '',
-			)
+			$args			
 		);
+		
+		if ( \is_wp_error( $response ) ) {
+			return new \WP_REST_Response( $response->get_error_message(), 503 );
+		}
 
 		return new \WP_REST_Response( $response );
 	}
