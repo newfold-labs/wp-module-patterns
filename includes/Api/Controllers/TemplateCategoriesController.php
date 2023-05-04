dc95f7712ea203ade0c94d9f09519388ea52c2b0
zlateska <hzlateska@gmail.com>
2023-05-04 11:32:29 +0200

    Update TemplateCategoriesController.php

--- includes/Api/Controllers/TemplateCategoriesController.php
+++ includes/Api/Controllers/TemplateCategoriesController.php
@@ -1,7 +1,7 @@
 <?php
 namespace NewfoldLabs\WP\Module\Patterns\Api\Controllers;
 
-use NewfoldLabs\WP\Module\Patterns\Api\RemoteRequest;
+use NewfoldLabs\WP\Module\Patterns\Library\Categories;
 
 class TemplateCategoriesController {
 
@@ -10,12 +10,7 @@ class TemplateCategoriesController {
 	 */
 	public static function index() {
 		
-		$categories = get_transient( 'wba_template_categories' );
-
-		if ( false === $categories ) {
-			$categories = RemoteRequest::get( '/templateCategories' );
-			set_transient( 'wba_template_categories', $categories, 60 * 60 * 24 );
-		}
+		$categories = Categories::get( 'templates' );
 
 		return new \WP_REST_Response( $categories );
 	}
