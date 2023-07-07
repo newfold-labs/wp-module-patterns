f3bc11e4a8407bef6dcb1722e879e4b040f2eb9a
arunshenoy99 <devarunshenoy99@gmail.com>
2023-07-07 17:41:44 +0530

    upgrade module to 0.1.1

--- build/0.1.0/wonder-blocks.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => 'e639b0886b1fa4a38068');
