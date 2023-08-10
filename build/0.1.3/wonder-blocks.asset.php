730b845cba96eacee9a7c44d64031f92c4a9dd49
zlateska <hzlateska@gmail.com>
2023-08-10 12:21:27 -0400

    bump version

---
+++ build/0.1.3/wonder-blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => '682a43868cb289e50f99');
