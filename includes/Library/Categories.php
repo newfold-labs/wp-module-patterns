87898a8e831c1eb417696c987549c4c51a46966f
Branko Conjic <brankoconjic@gmail.com>
2023-08-14 16:09:25 -0400

    Get Template Categories from Hiive

--- includes/Library/Categories.php
+++ includes/Library/Categories.php
@@ -24,19 +24,15 @@ class Categories {
 		// Get the categories from the transient.
 		$data = \get_transient( "wba_{$type}_categories" );
 
-		// If the transient is empty, get the categories from the remote API.
+		// If the transient is empty or if we are in dev mode get the categories from the remote API.
 		if ( false === $data || ( \defined( 'NFD_WB_DEV_MODE' ) && NFD_WB_DEV_MODE ) ) {
 
-			if ( 'templates' === $type ) {
-				$data = self::get_template_categories();
-			} else {
-				$data = RemoteRequest::get(
-					'/categories',
-					array(
-						'type' => $type,
-					)
-				);
-			}
+			$data = RemoteRequest::get(
+				'/categories',
+				array(
+					'type' => $type,
+				)
+			);
 
 			if ( \is_wp_error( $data ) ) {
 				return $data;
@@ -46,7 +42,8 @@ class Categories {
 				$data = $data['data'];
 			}
 
-			$data = self::add_featured_category( $data );
+			// Temporarily add the featured category until API returns this.
+			$data = self::add_featured_category( $data, $type );
 
 			\set_transient( "wba_{$type}_categories", $data, DAY_IN_SECONDS );
 		}
@@ -56,70 +53,14 @@ class Categories {
 	}
 
 	/**
-	 * Get template categories from the templates array.
-	 *
-	 * @return array $categories Array of template categories.
-	 */
-	private static function get_template_categories() {
-
-		// Get the templates.
-		$templates  = Items::get( 'templates' );
-		$categories = array();
-
-		// Return the empty categories array if there is an error.
-		if ( \is_wp_error( $templates ) ) {
-			return $categories;
-		}
-
-		// Iterate through the templates and get the categories.
-		if ( is_array( $templates ) && ! empty( $templates ) ) {
-			foreach ( $templates as $template ) {
-
-				// Check if the template belongs to categories.
-				if ( isset( $template['categories'] ) ) {
-
-					// Ensure the categories are an array.
-					$_categories = is_array( $template['categories'] ) ? $template['categories'] : explode( ',', str_replace( ' ', '', $template['categories'] ) );
-
-					foreach ( $_categories as $_category_slug ) {
-
-						$category_slug = sanitize_text_field( $_category_slug );
-
-						$category_item = array(
-							'id'    => md5( $category_slug ),
-							'title' => $category_slug,
-							'label' => ucwords( str_replace( '-', ' ', $category_slug ) ),
-						);
-
-						if ( ! isset( $categories[ $category_item['id'] ] ) ) {
-							$categories[ $category_item['id'] ] = $category_item;
-						}
-					}
-				}
-			}
-		}
-
-		// Get only the values of the categories.
-		if ( ! empty( $categories ) ) {
-			$categories = array_values( $categories );
-		}
-
-		return $categories;
-	}
-
-	/**
 	 * Manually add the featured category to the categories array.
+	 * This is a temporary solution until HIIVE returns the actual data.
 	 *
-	 * @param array $data Array of categories.
+	 * @param array  $data Array of categories.
+	 * @param string $type Type of categories to get.
 	 * @return array $data Array of categories with the featured category.
 	 */
-	private static function add_featured_category( $data ) {
-
-		$featured_category = array(
-			'id'    => '99262afa-322a-4f3c-92e4-c297ba153763',
-			'title' => 'featured',
-			'label' => 'Featured',
-		);
+	private static function add_featured_category( $data, $type ) {
 
 		$data = array_filter(
 			$data,
@@ -128,8 +69,34 @@ class Categories {
 			}
 		);
 
+		$id = self::generateUuidV4();
+
+		$featured_category = array(
+			'id'    => $id,
+			'title' => 'featured',
+			'label' => 'Featured',
+			'count' => $type === 'templates' ? 4 : 11,
+		);
+
 		$data = array_merge( array( $featured_category ), $data );
 
 		return $data;
+	}
+
+	/**
+	 * Temporary solution to add UUIDs to manual categories.
+	 */
+	private static function generateUuidV4() {
+		return sprintf(
+			'%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0x0fff ) | 0x4000,
+			mt_rand( 0, 0x3fff ) | 0x8000,
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff )
+		);
 	}
 }
