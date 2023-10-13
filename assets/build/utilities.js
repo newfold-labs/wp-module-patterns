fce82b795410e365c56c5533468b38fd3edc3c2a
Branko Conjic <brankoconjic@gmail.com>
2023-10-13 17:52:25 -0400

    Add build

---
+++ assets/build/utilities.js
@@ -0,0 +1,59 @@
+(() => {
+  // assets/scripts/viewportAnimationObserver.js
+  var ViewportAnimationObserver = class {
+    constructor(options = {}) {
+      this.options = {
+        activeClass: "nfd-wb-in-viewport",
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5,
+        ...options
+      };
+    }
+    /**
+     * Observe elements to trigger animations.
+     *
+     * @param {NodeList} elements - Elements to observe.
+     */
+    observeElements(elements) {
+      if (!("IntersectionObserver" in window)) {
+        return;
+      }
+      const observer = new IntersectionObserver(
+        this._handleIntersection.bind(this),
+        this.options
+      );
+      elements.forEach((element) => observer.observe(element));
+    }
+    /**
+     * Handle intersection events to trigger animations.
+     *
+     * @param {Array<IntersectionObserverEntry>} entries  - Intersection entries.
+     * @param {IntersectionObserver}             observer - The observer instance.
+     * @private
+     */
+    _handleIntersection(entries, observer) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(this.options.activeClass);
+          observer.unobserve(entry.target);
+        }
+      });
+    }
+  };
+
+  // assets/scripts/utilities.js
+  document.addEventListener("DOMContentLoaded", () => {
+    viewportAnimation();
+  });
+  function viewportAnimation() {
+    const viewportAnimationObserver = new ViewportAnimationObserver({
+      // activeClass: 'nfd-wb-in-viewport'
+      threshold: 0.2
+      // at least 20% of the element is in the viewport
+    });
+    const elementsToAnimate = document.querySelectorAll(".nfd-wb-animate");
+    viewportAnimationObserver.observeElements(elementsToAnimate);
+  }
+})();
+//# sourceMappingURL=utilities.js.map
--- build/0.1.5/wonder-blocks.asset.php
+++ build/0.1.5/wonder-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '9d7d5545f1fe4901b67f');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'eb2ebe989422e7572657');
