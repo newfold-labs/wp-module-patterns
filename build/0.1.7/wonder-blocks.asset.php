8cf1e5173a24949f80cf9800d802ac3e5b44c8c8
zlateska <hzlateska@gmail.com>
2023-10-24 07:06:15 -0400

    v0.1.7

---
+++ build/0.1.7/wonder-blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '753b982cef726c72412b');
