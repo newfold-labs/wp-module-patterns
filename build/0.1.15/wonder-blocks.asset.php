522653dec48b562e693db218a7d6b3936865bb60
Branko Conjic <brankoconjic@gmail.com>
2024-03-26 11:45:21 -0400

    Update dependencies (#59)

--- build/0.1.15/wonder-blocks.asset.php
+++ build/0.1.15/wonder-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '19d0efa673d3f379e54f');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'a01d4d9cba957404cc25');
