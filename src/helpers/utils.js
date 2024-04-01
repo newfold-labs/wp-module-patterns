199133082c46218de14b38057130bcb1d2f2d067
Branko Conjic <brankoconjic@gmail.com>
2024-04-01 15:42:55 -0400

    Prettier

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -7,15 +7,15 @@ export function formatVersion(version) {
 	}
 	/* For a version that looks like 1.2.3-RC1, numericVersion = "1.2.3" rcSuffix = "RC1" */
 	const [numericVersion, rcSuffix] = version.split(/-(.+)/);
-	const versionParts = numericVersion.split('.');
+	const versionParts = numericVersion.split(".");
 
 	while (versionParts.length < 3) {
-		versionParts.push('0');
+		versionParts.push("0");
 	}
 
 	const formattedVersion = rcSuffix
-		? `${versionParts.join('.')}-${rcSuffix}`
-		: versionParts.join('.');
+		? `${versionParts.join(".")}-${rcSuffix}`
+		: versionParts.join(".");
 
 	return formattedVersion;
 }
