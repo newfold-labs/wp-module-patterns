d40c7f4b91c85836c8a2335a3cec03c706436a07
zlateska <hzlateska@gmail.com>
2024-05-07 11:12:45 -0400

    Update logo to rectangle-group icon

--- src/components/ToolbarButton.jsx
+++ src/components/ToolbarButton.jsx
@@ -8,7 +8,8 @@ import classNames from "classnames";
  */
 import { ToolbarButton as WPToolbarButton } from "@wordpress/components";
 import { useDispatch, useSelect } from "@wordpress/data";
-import { Icon, buttons } from "@wordpress/icons";
+import { Icon } from "@wordpress/icons";
+import { rectangleGroup } from "./Icons";
 
 /**
  * Internal dependencies
@@ -26,9 +27,9 @@ const ToolbarButton = () => {
 
 	return (
 		<WPToolbarButton
-			icon={<Icon icon={buttons} />}
+			icon={<Icon icon={rectangleGroup} />}
 			className={classNames(
-				"nfd-wba-mr-2 nfd-wba-flex !nfd-wba-h-9 !nfd-wba-min-w-[36px] nfd-wba-shrink-0 nfd-wba-bg-brand !nfd-wba-p-0 nfd-wba-text-white hover:nfd-wba-bg-brand-darker hover:nfd-wba-text-white focus-visible:nfd-wba-text-white active:nfd-wba-bg-brand-darker-10 active:!nfd-wba-text-white lg:!nfd-wba-pl-3 lg:!nfd-wba-pr-[15px]",
+				"nfd-wba-gap-1 nfd-wba-mr-2 nfd-wba-flex !nfd-wba-h-9 !nfd-wba-min-w-[36px] nfd-wba-shrink-0 nfd-wba-bg-brand !nfd-wba-p-0 nfd-wba-text-white hover:nfd-wba-bg-brand-darker hover:nfd-wba-text-white focus-visible:nfd-wba-text-white active:nfd-wba-bg-brand-darker-10 active:!nfd-wba-text-white lg:!nfd-wba-pl-3 lg:!nfd-wba-pr-[15px]",
 				isModalOpen && "!nfd-wba-bg-dark nfd-wba-text-white"
 			)}
 			isPressed={isModalOpen}
