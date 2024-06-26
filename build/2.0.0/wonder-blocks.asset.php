b237c215c1c3758d8854a17b9470f8501613958e
zlateska <hzlateska@gmail.com>
2024-06-26 11:44:08 -0400

    bump version to 2.1.0

--- build/2.0.0/wonder-blocks.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => 'e7dfb18b40a98d2b98fd');
