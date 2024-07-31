1b19a0268fdc8d6ef49cc44f963efd77333f1e0a
Vara <varaprasadmaruboina@gmail.com>
2024-07-31 15:53:49 +0530

    Add validation to ensure label key is present before sending event to Hiive (#91)
    * Add a validation check for the Hiive event label key
    
    * Add console.error
    
    * Add console.error

--- src/helpers/analytics.js
+++ src/helpers/analytics.js
@@ -2,6 +2,14 @@ import { HiiveAnalytics, HiiveEvent } from "@newfold-labs/js-utility-ui-analytic
 import { HIIVE_ANALYTICS_CATEGORY } from "../constants";
 
 export const trackHiiveEvent = (action, data) => {
+	// Check if the label key for the event is present and not empty
+	const labelKey = data.label_key;
+	if (labelKey && !data[labelKey]) {
+		// eslint-disable-next-line no-console
+		console.error(`Missing or empty '${labelKey}' in Hiive event data`, data);
+		return;
+	}
+	
 	data = {
 		...data,
 		page: window.location.href, // todo: check if this is what we want.
