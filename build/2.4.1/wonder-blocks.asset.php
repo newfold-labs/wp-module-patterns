35dd32c990d3a4c381dfe01f1c7c446f1e4dc1cb
zlateska <hzlateska@gmail.com>
2024-09-04 09:48:55 -0400

    Version 2.4.1

---
+++ build/2.4.1/wonder-blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => 'e7e3a736aaac9a30e52b');
