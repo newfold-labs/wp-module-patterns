4094d885be158a852679d75f23b536a50e54d948
Evan Mullins <evanrm@gmail.com>
2025-03-03 16:04:31 -0500

    add release script, run release script to set version 2.9.0, update build files, and language files

---
+++ build/2.9.0/wonder-blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '951abe106f446d01dcfd');
